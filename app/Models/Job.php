b4e0f42d305486e7400cf982de6e8cb27ade27be
Heltzel <heltzel.service@gmail.com>
2024-05-15 14:58:49 +0200

    Eloquent section up to hasMany relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,37 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return
-            [
-                [
-                    'id' => 1,
-                    'title' => 'Director',
-                    'salary' => '$50,000'
-                ],
-                [
-                    'id' => 2,
-                    'title' => 'Developer',
-                    'salary' => '$25,000'
-                ],
-                [
-                    'id' => 3,
-                    'title' => 'Teacher',
-                    'salary' => '$45,000'
-                ]
-            ];
-    }
+    use HasFactory;
+    protected $table = "job_listing";
+    protected $fillable = ['employer_id', 'title', 'salary'];
 
-    public static function find(int $id): array
+    public function employer()
     {
-        $job = Arr::first(static::all(), fn ($job) => $job['id'] == $id);
-        if (!$job) {
-            abort(404);
-        }
-        return $job;
+        return $this->belongsTo(Employer::class);
     }
 }
