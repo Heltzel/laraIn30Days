a86a6d2279960c03d2c593907462f3335cc669a2
Heltzel <heltzel.service@gmail.com>
2024-05-20 15:26:03 +0200

    Homework: Create many to many relation Post to Tag

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\Job;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -14,5 +15,9 @@ class Tag extends Model
     public function jobs()
     {
         return $this->belongsToMany(Job::class, relatedPivotKey: 'job_listing_id')->withTimestamps();
+    }
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class)->withTimestamps();
     }
 }
